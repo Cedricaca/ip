6bbde7461b9740e7cc4a3ecf9615f4b31b1c0f8a
Cedricaca <>
2024-03-19 12:48:55 +0800

    Changed some tests to align with requirements

--- src/test/java/duke/StorageTest.java
+++ src/test/java/duke/StorageTest.java
@@ -5,6 +5,7 @@ public class StorageTest {
     @Test
     public void storageTest() {
         Storage s = new Storage();
+        s.clear();
         s.add("testString");
         assertEquals(s.read(0), "testString");
         s.edit(0, "nani");
